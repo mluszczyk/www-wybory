820d34722d039ff72eab3edbbf4e7dfb36877274
Michał Łuszczyk <michal.luszczyk@gmail.com>
2016-04-06 13:10:14 +0200

    Disabled modifying the number of candidates in the admin.

--- wyniki/admin.py
+++ wyniki/admin.py
@@ -3,4 +3,19 @@ from wyniki import models
 
 admin.site.register(models.Wojewodztwo)
 admin.site.register(models.Gmina)
-admin.site.register(models.Kandydat)
+
+
+class KandydatAdmin(admin.ModelAdmin):
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    def get_actions(self, request):
+        actions = super().get_actions(request)
+        del actions['delete_selected']
+        return actions
+
+
+admin.site.register(models.Kandydat, KandydatAdmin)
