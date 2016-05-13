d678bce689c0a5af89374f8453a76545fe424bd0
Michał Łuszczyk <michal.luszczyk@gmail.com>
2016-05-13 13:17:44 +0200

    New selenium tests.

--- wyniki/tests/test_selenium.py
+++ wyniki/tests/test_selenium.py
@@ -1,11 +1,17 @@
-from django import test
+import unittest
+
+import time
+from django.contrib.auth.models import User
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 
 from wyniki import models
 from wyniki.tests import factories
 
 
-class SeleniumTest(test.LiveServerTestCase):
+class SeleniumTest(StaticLiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Chrome()
         self.candidates = [
@@ -24,3 +30,33 @@ class SeleniumTest(test.LiveServerTestCase):
         xpath = "/html/body/div[5]/div/div[2]/table/tbody/tr[2]/td[3]"
         element = self.browser.find_element_by_xpath(xpath)
         self.assertEquals(element.text, "1000")
+
+    def test_commune_list_unavailable_not_logged(self):
+        self.browser.get(self.live_server_url)
+
+        xpath = "//span[@data-code='miasto']"
+        element = self.browser.find_element_by_xpath(xpath)
+        element.click()
+
+        with self.assertRaises(NoSuchElementException):
+            self.browser.find_element_by_class_name("edit-results")
+
+    def test_commune_list(self):
+        User.objects.create_user("blah", None, "blah")
+
+        self.browser.get(self.live_server_url)
+        self.browser.find_element_by_xpath("//input[@name='username']").send_keys("blah")
+        self.browser.find_element_by_xpath("//input[@name='password']").send_keys("blah")
+        self.browser.find_element_by_xpath("//input[@name='submit']").click()
+
+        time.sleep(0.1)
+
+        self.assertIn("Dzień dobry", self.browser.find_element_by_tag_name("body").text)
+
+        xpath = "//span[@data-code='miasto']"
+        element = self.browser.find_element_by_xpath(xpath)
+        element.click()
+
+        time.sleep(0.1)
+
+        self.browser.find_element_by_xpath("//span[@class='edit-results']")
